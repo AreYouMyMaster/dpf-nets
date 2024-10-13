ebb763ecdaa33e3576478e612be7c8f21f8c1f53
boillobster <robin0412@hotmail.com>
2024-10-13 20:26:33 +0800

    Edit path2save

--- unit_test/DataLoaderTest.py
+++ unit_test/DataLoaderTest.py
@@ -2,6 +2,8 @@ import sys
 import os
 import numpy as np
 import unittest
+import torch
+from lib.networks.utils import save_model
 
 from lib.datasets.pointflowDataLoader import (
     PointflowDataLoader,
@@ -18,6 +20,14 @@ class TestDataLoader(unittest.TestCase):
         all_path.sort()
         print(all_path[0])
         self.assertEqual(all_path[0], "/rawdata3/ShapeNetCore.v2.PC15k/02691156/train/10155655850468db78d106ce0a280f87.npy")
+
+    def test_save_data(self):
+        a = torch.tensor([0,1,2,3,4])
+        path = './data/models/DPFNets/a'
+        save_model(a, path)
+        self.assertTrue(os.path.exists(path))
+        if(os.path.exists(path)):
+            os.remove(path)
 
 
 if __name__ == "__main__":
